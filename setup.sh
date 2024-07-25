6841cbf15bee2621214caf8ecec531c420281b18
saengx <157899625+saengx@users.noreply.github.com>
2024-07-26 04:36:13 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -28,6 +28,10 @@ apt-get install figlet -y
 apt-get install python3-progress -y
 apt-get install python3-requests -y
 
+apt-get install -y libllvm-16-ocaml-dev libllvm16 llvm-16 llvm-16-dev llvm-16-doc llvm-16-examples llvm-16-runtime clang-16 clang-tools-16 clang-16-doc libclang-common-16-dev libclang-16-dev libclang1-16 clang-format-16 python3-clang-16 clangd-16 clang-tidy-16 libclang-rt-16-dev libpolly-16-dev libfuzzer-16-dev lldb-16 lld-16 libc++-16-dev libc++abi-16-dev libomp-16-dev libclc-16-dev libunwind-16-dev libmlir-16-dev mlir-16-tools flang-16 libclang-rt-16-dev-wasm32 libclang-rt-16-dev-wasm64 libclang-rt-16-dev-wasm32 libclang-rt-16-dev-wasm64
+
+apt-get install clang-format clang-tidy clang-tools clang clangd libc++-dev libc++1 libc++abi-dev libc++abi1 libclang-dev libclang1 liblldb-dev libllvm-ocaml-dev libomp-dev libomp5 lld lldb llvm-dev llvm-runtime llvm python3-clang -y
+
 mv mobile-mining ../../etc
 mv edit-miner ../../bin
 mv run-miner ../../bin
@@ -46,7 +50,7 @@ cd && cd ../etc/mobile-mining/ccminer
 chmod +x build.sh
 chmod +x configure.sh
 chmod +x autogen.sh
-./build.sh
+CXX=clang++ CC=clang ./build.sh
 
 chmod +x ccminer
 
