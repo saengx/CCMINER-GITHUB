1b4feb848266fb02955a2a04636f206065f56033
saengx <157899625+saengx@users.noreply.github.com>
2025-01-22 07:03:53 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -42,6 +42,7 @@ mv ANSI_Shadow.flf ../../usr/share/figlet
 mv backup /data/data/com.termux/files/usr/bin
 mv restore /data/data/com.termux/files/usr/bin
 mv install.txt /storage/emulated/0/download
+mv cpuminer /data/data/com.termux/files/usr/bin
 
 run-miner
 
