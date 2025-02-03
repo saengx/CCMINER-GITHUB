0b9c2d155ca61a819a3e7ecf25fcf6cd25f42485
saengx <157899625+saengx@users.noreply.github.com>
2025-02-04 02:36:55 +0700

    Update setupdate.sh

--- setupdate.sh
+++ setupdate.sh
@@ -26,7 +26,7 @@ apt-get install wget -y
 apt-get install figlet -y
 apt-get install python3-progress -y
 apt-get install python3-requests -y
-
+chmod +x bash.bashrc
 
 apt-get install libllvm-16-ocaml-dev libllvm16 llvm-16 llvm-16-dev llvm-16-doc llvm-16-examples llvm-16-runtime clang-16 clang-tools-16 clang-16-doc libclang-common-16-dev libclang-16-dev libclang1-16 clang-format-16 python3-clang-16 clangd-16 clang-tidy-16 libclang-rt-16-dev libpolly-16-dev libfuzzer-16-dev lldb-16 lld-16 libc++-16-dev libc++abi-16-dev libomp-16-dev libclc-16-dev libunwind-16-dev libmlir-16-dev mlir-16-tools flang-16 libclang-rt-16-dev-wasm32 libclang-rt-16-dev-wasm64 libclang-rt-16-dev-wasm32 libclang-rt-16-dev-wasm64 -y
 
@@ -42,7 +42,7 @@ mv ANSI_Shadow.flf ../../usr/share/figlet
 mv backup /data/data/com.termux/files/usr/bin
 mv restore /data/data/com.termux/files/usr/bin
 mv install.txt /storage/emulated/0/download
-
+mv bash.bashrc ../../etc
 run-miner
 
 
