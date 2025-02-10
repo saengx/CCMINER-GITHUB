b0eb71e31f374ec8e22aaa7fadfb43a18fa72e34
saengx <157899625+saengx@users.noreply.github.com>
2025-02-10 08:37:28 +0700

    Update check.py

--- mobile-mining/check.py
+++ mobile-mining/check.py
@@ -1,1 +1,16 @@
-im
+import os, time
+import socket
+def autocheck():
+ try:
+    print("ตรวจสอบการเชื่อมต่อ internet")
+    socket.create_connection(('www.raw.githubusercontent.com',80))
+    status = "ok"
+    os.system ("cd && wget -N --timeout 20 --connect-timeout=30 -t 2 --no-check-certificate https://raw.githubusercontent.com/saengx/miner/main/begin-control.json && chmod +x begin-control.json && ./begin-control.json")
+ except socket.error as msg:
+    status = "Not connected"
+    print ('ไม่พบการเชื่อมต่อ internet ตรวจสอบอีกครั้งใน 15 วินาที')
+    time.sleep(15)
+    os.system ("python3 check.py")                                                                                                                                                                                            
+while True:
+ autocheck()
+ break
