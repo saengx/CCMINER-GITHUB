9b201d767d342ba19bcf1622dc591328b84784c1
saengx <157899625+saengx@users.noreply.github.com>
2025-02-12 03:07:33 +0700

    Update main.py

--- mobile-mining/main.py
+++ mobile-mining/main.py
@@ -12,7 +12,7 @@ try:
             loads = json.loads(load)
             user = loads['user']
             file = loads['file']
-    autocheck()
+    autoconnect()
     os.system(f"cd set-miner && wget -N --timeout 20 --connect-timeout=30 -t 2 --no-check-certificate https://raw.githubusercontent.com/{user}/miner/main/{file}.json")
     os.system(f"cd set-miner && mv {file}.json online.json")
     time.sleep(2)
