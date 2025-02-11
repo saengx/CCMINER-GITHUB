1e52dcf15fcfaee6b780ee71c3bc383d02f80b8e
saengx <157899625+saengx@users.noreply.github.com>
2025-02-12 03:07:05 +0700

    Update connect.py

--- mobile-mining/connect.py
+++ mobile-mining/connect.py
@@ -1,15 +1,16 @@
 import os, time, json
 import socket
-def autocheck():
+def autoconnect():
  try:
      with open("setip/ip.json", encoding="utf-8") as set:
              load = set.read()
              loads = json.loads(load)
              user = loads['user']
+             file = loads['file']
              print("\n\033[96mตรวจสอบการเชื่อมต่อกับ GITHUB\033[0m\n")
              socket.create_connection(('www.raw.githubusercontent.com',80))
              status = "ok"
-             os.system (f"cd && wget -N --timeout 20 --connect-timeout=30 -t 2 --no-check-certificate https://raw.githubusercontent.com/{user}/miner/main/begin-control.json && chmod +x begin-control.json && ./begin-control.json")
+             os.system(f"cd set-miner && wget -N --timeout 20 --connect-timeout=30 -t 2 --no-check-certificate https://raw.githubusercontent.com/{user}/miner/main/{file}.json")
  except socket.error as msg:
     status = "Not connected"
     print ('\n\033[95mไม่พบการเชื่อมต่อ ตรวจสอบอีกครั้งใน 15 วินาที\033[0m\n')
