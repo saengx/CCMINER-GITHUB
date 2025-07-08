91ac260929bf9575fddffc04bb9fcdee0ddc2d7d
saengx <157899625+saengx@users.noreply.github.com>
2025-07-08 07:26:11 +0700

    Update connect.py

--- mobile-mining/connect.py
+++ mobile-mining/connect.py
@@ -9,7 +9,7 @@ def autoconnect():
              user = loads['user']
              file = loads['file']
              print("\n\033[92mตรวจสอบการเชื่อมต่อค่า config \033[0m\n")
-             response = urlopen('http://google.com')
+             response = urlopen('http://raw.githubusercontent.com')
              #socket.create_connection(('www.google.com',80))
              status = "ok"
              #os.system(f"cd set-miner && wget -N --timeout 20 --connect-timeout=30 -t 2 --no-check-certificate https://raw.githubusercontent.com/{user}/miner/main/{file}.json")
