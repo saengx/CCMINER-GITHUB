c90835ecca24ff6b044bf94596deffe13b6830a4
saengx <157899625+saengx@users.noreply.github.com>
2025-07-28 04:15:00 +0700

    Update cpu.py

--- mobile-mining/cpu.py
+++ mobile-mining/cpu.py
@@ -8,7 +8,7 @@ cpu_count = psutil.cpu_count(logical=False)
 logical_cpu_count = psutil.cpu_count(logical=True)
 system_info = platform.uname()
 
-print("\nCPU Information")
+print("\033[1;32;40mCPU Information")
 print(f"Processor: {cpu_info}")
 print(f"Physical Cores: {cpu_count}")
 print(f"Logical Cores: {logical_cpu_count}")
