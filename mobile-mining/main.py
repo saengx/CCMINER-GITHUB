eaedfe8495d65f0e2ca2a0928d0469e1c3c1990e
saengx <157899625+saengx@users.noreply.github.com>
2025-07-28 07:03:57 +0700

    Update main.py

--- mobile-mining/main.py
+++ mobile-mining/main.py
@@ -73,7 +73,7 @@ def runOffline():
            
            os.system(f"python3 cpu.py")
            #time.sleep(2)
-           os.system(f"cd ccminer && ./ccminer -a verus -o {pool} -u {wallet}.{name} -p {password},ID={name} -t {cpu}")
+           os.system(f"cd ccminer && ./ccminer -a verus -o {pool} -u {wallet}.{name} -p {password},ID={name} -t {cpu} --cpu-affinity -1 --cpu-priority 1 --api-allow=192.168.1.0/16")
        
         else:
         	
@@ -82,7 +82,7 @@ def runOffline():
          
          os.system(f"python3 cpu.py")
          #time.sleep(2)
-         os.system(f"cd ccminer && ./ccminer -a verus -o {pool} -u {wallet}.{name} -p {password} -t {cpu}")
+         os.system(f"cd ccminer && ./ccminer -a verus -o {pool} -u {wallet}.{name} -p {password} -t {cpu} --cpu-affinity -1 --cpu-priority 1 --api-allow=192.168.1.0/16")
     except:
         push = {'pool': '','wallet': '','pass': ''}
         with open("set-miner/{'file'}.json", "w") as set:
