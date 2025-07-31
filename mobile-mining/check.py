8b09aefdc9dc8774bf82479122073d9dc915b0e4
saengx <157899625+saengx@users.noreply.github.com>
2025-08-01 06:08:13 +0700

    Update check.py

--- mobile-mining/check.py
+++ mobile-mining/check.py
@@ -8,15 +8,15 @@ def autocheck(url, filename):
              response = requests.get (url, stream=True)
              response.raise_for_status() 
      with open(filename, 'wb') as f:
-         for chunk in response.iter_content(chunk>
+         for chunk in response.iter_content(chunk_size=8192):
              f.write(chunk)
-             print(f"\n\033[95mเชื่อมต่อสำเร็จแล้ว\033>
+             print(f"\n\033[95mเชื่อมต่อสำเร็จแล้ว\033[0m\n
 
  except requests.exceptions.RequestException as e:
-             print ('\n\033[95mไม่พบการเชื่อมต่อ ตรวจ>
-             time.sleep(15)
+             print ('\n\033[95mไม่พบการเชื่อมต่อ ตรวจสอบอีกครั้งใน 10 วินาที
+             time.sleep(1)
              os.system ("python3 check.py")
-url_to_download = "https://raw.githubusercontent.>
+url_to_download = "https://raw.githubusercontent.com/saengx/miner/main/begin-control.json"
 output_filename = "start"
 autocheck(url_to_download, output_filename)
 os.system ("chmod +x start && mv start ../../bin")
