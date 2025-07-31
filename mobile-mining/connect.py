365c6b1c3c085aa058a9a0da148ee6162be96b44
saengx <157899625+saengx@users.noreply.github.com>
2025-08-01 06:45:25 +0700

    Update connect.py

--- mobile-mining/connect.py
+++ mobile-mining/connect.py
@@ -1,26 +1,29 @@
 import os, time, json
-import socket
-from urllib.request import urlopen
-def autoconnect():
+import requests
+def autoconect(url, filename):
  try:
      with open("setip/ip.json", encoding="utf-8") as set:
              load = set.read()
              loads = json.loads(load)
-             user = loads['user']
-             file = loads['file']
-             print("\n\033[92mตรวจสอบการเชื่อมต่อค่า config \033[0m\n")
-             response = urlopen('http://raw.githubusercontent.com')
-             #socket.create_connection(('www.google.com',80))
-             status = "ok"
-             #os.system(f"cd set-miner && wget -N --timeout 20 --connect-timeout=30 -t 2 --no-check-certificate https://raw.githubusercontent.com/{user}/miner/main/{file}.json")
- except socket.error as msg:
-    status = "Not connected"
-    print ('\n\033[95mไม่พบการเชื่อมต่อ ตรวจสอบอีกครั้งใน 15 วินาที\033[0m\n')
-    time.sleep(15)
-    os.system ("python3 connect.py")                                                                                                                                                                                            
-while True:
-      autoconnect()
-      break
+             user = loads['user']        
+             print("\n\033[96mตรวจสอบการเชื่อมต่อกับ GITHUB\033[0m\n")
+             url = "https://raw.githubusercontent.com/saengx/miner/main/begin-control.json"
+             output_filename = "start"
+             response = requests.get (url, stream=True)
+             response.raise_for_status() 
+     with open(filename, 'wb') as f:
+         for chunk in response.iter_content(chunk_size=8192):
+             f.write(chunk)
+             print(f"\n\033[95mเชื่อมต่อสำเร็จแล้ว\033[0m\n")
+
+ except requests.exceptions.RequestException as e:
+             print ("\n\033[95mไม่พบการเชื่อมต่อ ตรวจสอบอีกครั้งใน 10 วินาที\033[0m\n")
+             time.sleep(1)
+             os.system ("python3 check.py")
+url_to_download = "https://raw.githubusercontent.com/saengx/miner/main/begin-control.json"
+output_filename = "start"
+autocheck(url_to_download, output_filename)
+
 import requests
 
 def download_file(url, filename):
