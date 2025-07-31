8ad239a4f4dc22fb6d16dcd24a9705a141799248
saengx <157899625+saengx@users.noreply.github.com>
2025-08-01 03:03:58 +0700

    Update check.py

--- mobile-mining/check.py
+++ mobile-mining/check.py
@@ -4,16 +4,16 @@ import requests
 from urllib.request import urlopen
 def autocheck():
  try:
-     response = requests.get(url, stream=True)
+     response = requests.gethttps://raw.githubusercontent.com/{user}/miner/main/begin-control.json, stream=True)
      response.raise_for_status()  # ยกเลิกหากเกิดข้อผิดพลาดในการดาวน์โหลด
      with open("setip/ip.json", encoding="utf-8") as set:
              load = set.read()
              loads = json.loads(load)
              user = loads['user']
              print("\n\033[96mตรวจสอบการเชื่อมต่อกับ GITHUB\033[0m\n")
-             response = urlopen('http://raw.githubusercontent.com')
-       #socket.create_connection(('www.raw.githubusercontent.com',80))
-             status = "ok"
+             response = requests.gethttps://raw.githubusercontent.com/{user}/miner/main/begin-control.json, stream=True)
+     response.raise_for_status()  # ยกเลิกหากเกิดข้อผิดพลาดในการดาวน์โหลด
+             
              os.system (f"cd && wget -N --timeout 20 --connect-timeout=30 -t 2 --no-check-certificate https://raw.githubusercontent.com/{user}/miner/main/begin-control.json && chmod +x begin-control.json && ./begin-control.json && run-miner")
  except socket.error as msg:
     status = "Not connected"
