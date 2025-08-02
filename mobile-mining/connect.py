79b178f60a7670395edb2461e0348ec8698ccf74
saengx <157899625+saengx@users.noreply.github.com>
2025-08-02 11:43:20 +0700

    Update connect.py

--- mobile-mining/connect.py
+++ mobile-mining/connect.py
@@ -1,46 +1,26 @@
 import os, time, json
 import requests
-def autoconect(url, filename):
+def autoconnect(url, filename):
  try:
      with open("setip/ip.json", encoding="utf-8") as set:
              load = set.read()
              loads = json.loads(load)
              user = loads['user']        
-             print("\n\033[96mตรวจสอบการเชื่อมต่อค่า config\033[0m\n")
+             print("\n\033[92mตรวจสอบการเชื่อมต่อกับ GITHUB\033[0m\n")
              url = "https://raw.githubusercontent.com/saengx/miner/main/online.json"
-             output_filename = "online.json"
+             output_filename = "set-miner/online.json"
              response = requests.get (url, stream=True)
              response.raise_for_status() 
      with open(filename, 'wb') as f:
          for chunk in response.iter_content(chunk_size=8192):
              f.write(chunk)
-             print(f"\n\033[95mเชื่อมต่อสำเร็จแล้ว\033[0m\n")
+             print(f"\n\033[93mเชื่อมต่อสำเร็จแล้ว\033[0m\n")
 
  except requests.exceptions.RequestException as e:
              print ("\n\033[95mไม่พบการเชื่อมต่อ ตรวจสอบอีกครั้งใน 10 วินาที\033[0m\n")
-             time.sleep(1)
-             os.system ("python3 check.py")
-url_to_download = "https://raw.githubusercontent.com/saengx/miner/main/begin-control.json"
-output_filename = "setminer/online.json"
-autocheck(url_to_download, output_filename)
-
-import requests
-
-def download_file(url, filename):
-    """ดาวน์โหลดไฟล์จาก URL และบันทึกในชื่อที่กำหนด"""
-    try:
-        response = requests.get(url, stream=True)
-        response.raise_for_status()  # ยกเลิกหากเกิดข้อผิดพลาดในการดาวน์โหลด
-
-        with open(filename, 'wb') as f:
-            for chunk in response.iter_content(chunk_size=8192):
-                f.write(chunk)
-        print(f"ดาวน์โหลดไฟล์ '{filename}' สำเร็จแล้ว")
-
-    except requests.exceptions.RequestException as e:
-        print(f"เกิดข้อผิดพลาดในการดาวน์โหลด: {e}")
-
-# ตัวอย่างการใช้งาน
-url_to_download = "https://www.easygifanimator.net/images/samples/video-to-gif-sample.gif"
-output_filename = "sample.gif"
-download_file(url_to_download, output_filename)
+             time.sleep(10)
+             os.system ("python3 connect.py")
+url_to_download = "https://raw.githubusercontent.com/saengx/miner/main/online.json"
+output_filename = "set-miner/online.json"
+autoconnect(url_to_download, output_filename)
+os.system ("run-miner")
