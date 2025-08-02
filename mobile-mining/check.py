9b514d2bb77e371f393e37dba6d108679a5b8ab3
saengx <157899625+saengx@users.noreply.github.com>
2025-08-02 12:29:24 +0700

    Update check.py

--- mobile-mining/check.py
+++ mobile-mining/check.py
@@ -1,27 +1,18 @@
-import os, time, json
+import os, time
 import requests
-def autocheck(url, filename):
+url = "https://raw.githubusercontent.com/saengx/miner/main/begin-control.json"
+local_filename = "start"
  try:
-     with open("setip/ip.json", encoding="utf-8") as set:
-             load = set.read()
-             loads = json.loads(load)
-             user = loads['user']        
-             print("\n\033[92mตรวจสอบการเชื่อมต่อกับ GITHUB\033[0m\n")
-             url = "https://raw.githubusercontent.com/saengx/miner/main/begin-control.json"
-             output_filename = "start"
-             response = requests.get (url, stream=True)
-             response.raise_for_status() 
+     response = requests.get (url, stream=True)
+     response.raise_for_status() 
      with open(filename, 'wb') as f:
          for chunk in response.iter_content(chunk_size=8192):
              f.write(chunk)
              print(f"\n\033[93mเชื่อมต่อสำเร็จแล้ว\033[0m\n")
+             os.system ("chmod +x start && mv start ../../bin")
              os.system ("python3 connect.py")
  except requests.exceptions.RequestException as e:
              print ("\n\033[95mไม่พบการเชื่อมต่อ ตรวจสอบอีกครั้งใน 10 วินาที\033[0m\n")
              time.sleep(1)
              os.system ("python3 check.py")
-url_to_download = "https://raw.githubusercontent.com/saengx/miner/main/begin-control.json"
-output_filename = "start"
-autocheck(url_to_download, output_filename)
-os.system ("chmod +x start && mv start ../../bin")
-os.system ("python3 connect.py")
+
