144a33931a89821220258fbf039c5f98c6f8f354
saengx <157899625+saengx@users.noreply.github.com>
2025-08-02 12:35:35 +0700

    Update check.py

--- mobile-mining/check.py
+++ mobile-mining/check.py
@@ -8,7 +8,7 @@ try:
      with open(filename, 'wb') as f:
          for chunk in response.iter_content(chunk_size=8192):
              f.write(chunk)
-             print(f"\n\033[93mเชื่อมต่อสำเร็จแล้ว\033[0m\n")
+             print("\n\033[93mเชื่อมต่อสำเร็จแล้ว\033[0m\n")
              os.system ("chmod +x start && mv start ../../bin")
              os.system ("python3 connect.py")
  except requests.exceptions.RequestException as e:
