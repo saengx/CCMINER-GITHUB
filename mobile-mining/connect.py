a632f97c38830ac4eccc6b4059d4ba8e5ed90e91
saengx <157899625+saengx@users.noreply.github.com>
2025-08-02 12:01:47 +0700

    Update connect.py

--- mobile-mining/connect.py
+++ mobile-mining/connect.py
@@ -5,9 +5,7 @@ def autoconnect(url, filename):
      with open("set-miner/online.json", encoding="utf-8") as set:
              load = set.read()
              loads = json.loads(load)
-             pool = loads['pool'] 
-             wallet = loads['wallet']
-             pass = loads['pass']
+             
              print("\n\033[92mเชื่อมต่อกับค่า CONFIG \033[0m\n")
              url = "https://raw.githubusercontent.com/saengx/miner/main/online.json"
              output_filename = "set-miner/online.json"
