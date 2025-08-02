ea2eddc3e2fbdaedfd5fd8963f1cceb86e29334c
saengx <157899625+saengx@users.noreply.github.com>
2025-08-02 11:44:53 +0700

    Update main.py

--- mobile-mining/main.py
+++ mobile-mining/main.py
@@ -13,8 +13,8 @@ try:
             loads = json.loads(load)
             user = loads['user']
             file = loads['file']
-    autoconnect()
-    os.system(f"cd set-miner && wget -N --timeout 20 --connect-timeout=30 -t 2 --no-check-certificate https://raw.githubusercontent.com/{user}/miner/main/{file}.json")
+    #autoconnect()
+    #os.system(f"cd set-miner && wget -N --timeout 20 --connect-timeout=30 -t 2 --no-check-certificate https://raw.githubusercontent.com/{user}/miner/main/{file}.json")
     os.system(f"cd set-miner && mv {file}.json online.json")
     time.sleep(2)
     from progress.bar import ChargingBar
