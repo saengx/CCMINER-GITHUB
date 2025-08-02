7fcb1de5e4976f37c2996beeb6d1e0d5f48b910f
saengx <157899625+saengx@users.noreply.github.com>
2025-08-02 12:06:01 +0700

    Update check.py

--- mobile-mining/check.py
+++ mobile-mining/check.py
@@ -15,7 +15,7 @@ def autocheck(url, filename):
          for chunk in response.iter_content(chunk_size=8192):
              f.write(chunk)
              print(f"\n\033[93mเชื่อมต่อสำเร็จแล้ว\033[0m\n")
-
+             os.system ("python3 connect.py")
  except requests.exceptions.RequestException as e:
              print ("\n\033[95mไม่พบการเชื่อมต่อ ตรวจสอบอีกครั้งใน 10 วินาที\033[0m\n")
              time.sleep(1)
