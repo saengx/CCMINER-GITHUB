ca9ebfcc6e91178d1726b57551033811d2402b5f
saengx <157899625+saengx@users.noreply.github.com>
2025-08-16 05:48:40 +0700

    Update check.py

--- mobile-mining/check.py
+++ mobile-mining/check.py
@@ -27,7 +27,7 @@ with open("setip/ipserver.json", encoding="utf-8") as set:
              loads = json.loads(load)
              ip = loads['ip']
              IP = f"{ip}"
-file_url = f"http://{IP}:8080/start"
+file_url = f"http://{IP}:8080/start.json"
 #file_url = "https://raw.githubusercontent.com/saengx/miner/main/start"
 local_save_path = "start.json"
 download_file(file_url,local_save_path) 
