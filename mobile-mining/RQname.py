ff1cf8d55c75429f369fb78fb39bfd4c1829be05
saengx <157899625+saengx@users.noreply.github.com>
2025-08-24 20:43:04 +0700

    Update RQname.py

--- mobile-mining/RQname.py
+++ mobile-mining/RQname.py
@@ -4,14 +4,14 @@ import os, time, json
              json_string = json.dumps(data, ensure_ascii=False, indent=4)
              name = name
              NAME = f"{name}"
-   
-             __title__ = 'requests'
-             __description__ = 'Python HTTP for Humans.'
-             __url__ = 'https://requests.readthedocs.io'
-             __version__ = f'------{NAME}------'
-             __build__ = 0x022501
-             __author__ = 'Kenneth Reitz'
-             __author_email__ = 'me@kennethreitz.org'
-             __license__ = 'Apache 2.0'
-             __copyright__ = 'Copyright 2020 Kenneth Reitz'
-             __cake__ = u'\u2728 \U0001f370 \u2728'  
+             print(NAME)
+             #__title__ = 'requests'
+             #__description__ = 'Python HTTP for Humans.'
+             #__url__ = 'https://requests.readthedocs.io'
+             #__version__ = f'------{NAME}------'
+             #__build__ = 0x022501
+             #__author__ = 'Kenneth Reitz'
+             #__author_email__ = 'me@kennethreitz.org'
+             #__license__ = 'Apache 2.0'
+             #__copyright__ = 'Copyright 2020 Kenneth Reitz'
+             #__cake__ = u'\u2728 \U0001f370 \u2728'  
