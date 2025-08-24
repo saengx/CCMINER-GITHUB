7bed1d39ce041821b5dd080c3feec490de901c80
saengx <157899625+saengx@users.noreply.github.com>
2025-08-25 05:43:29 +0700

    Update RQname.py

--- mobile-mining/RQname.py
+++ mobile-mining/RQname.py
@@ -1,17 +1,30 @@
 import os, time, json
-   with open('set-miner/offline.json', 'r', encoding='utf-8') as f:
-             data = json.load(f)
-             json_string = json.dumps(data, ensure_ascii=False, indent=4)
-             name = name
-             NAME = f"{name}"
-             print(NAME)
-             #__title__ = 'requests'
-             #__description__ = 'Python HTTP for Humans.'
-             #__url__ = 'https://requests.readthedocs.io'
-             #__version__ = f'------{NAME}------'
-             #__build__ = 0x022501
-             #__author__ = 'Kenneth Reitz'
-             #__author_email__ = 'me@kennethreitz.org'
-             #__license__ = 'Apache 2.0'
-             #__copyright__ = 'Copyright 2020 Kenneth Reitz'
-             #__cake__ = u'\u2728 \U0001f370 \u2728'  
+with open("set-miner/offline.json", encoding="utf-8") as set:
+    load = set.read()
+    loads = json.loads(load)
+    name = loads['name']
+    NAME = f'{name}'
+    __title__ = 'requests'
+    __description__ = 'Python HTTP for Humans.'
+    __url__ = 'https://requests.readthedocs.io'
+    __version__ = f"'------{NAME}------'"
+    __build__ = 0x022501
+    __author__ = 'Kenneth Reitz'
+    __author_email__ = 'me@kennethreitz.org'
+    __license__ = 'Apache 2.0'
+    __copyright__ = 'Copyright 2020 Kenneth Reitz'
+    __cake__ = u'\u2728 \U0001f370 \u2728'
+    #print(f'{__version__}')
+    f = open("__version__.py", mode='w+', encoding="utf-8")
+    print("__title__ = 'requests'", file=f)
+    print("__description__ = 'Python HTTP for Humans.'", file=f)
+    print("__url__ = 'https://requests.readthedocs.io'", file=f)
+    print("__version__ = ",f"{__version__}", file=f)
+    print("__build__ = 0x022501", file=f)
+    print("__author__ = 'Kenneth Reitz'", file=f)
+    print("__author_email__ = 'me@kennethreitz.org'", file=f)
+    print("__license__ = 'Apache 2.0'", file=f)
+    print("__copyright__ = 'Copyright 2020 Kenneth Reitz'", file=f)
+    print("__cake__ = u'\u2728 \U0001f370 \u2728'", file=f)
+
+  
