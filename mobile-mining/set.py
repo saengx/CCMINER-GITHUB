f23754da905b958199de88e7b548720da5f7c977
saengx <157899625+saengx@users.noreply.github.com>
2025-09-10 17:59:44 +0700

    Update set.py

--- mobile-mining/set.py
+++ mobile-mining/set.py
@@ -11,23 +11,13 @@ def setminer():
        print("0-5 \033[93mปกติ 3\033[00m")
        cpupriority = input("[cpu-priority]: ")
        print("\033[35m-----------------------------------------\033[0m")
-
-       print(f"ค่า full-acess ip \033[93mเช่น 192.168.1.0/24\033[00m")
-       apiallow = input("[api-allow]: ")
-       print("\033[35m-----------------------------------------\033[0m")
-
-       print(f"ค่า bind ip:port \033[93mเช่น 0.0.0.0:4068\033[00m")
-       apibind = input("[api-bind]: ")
-       print("\033[35m-----------------------------------------\033[0m")
    except:
             os.system("@cls||clear")
             print("เกิดข้อผิดพลาดโปรดตั้งค่าใหม่!")
             time.sleep(3)
             os.system("set")
    push = {
-          'cpu-priority': cpupriority,
-          'api-allow': apiallow,
-          'api-bind' : apibind
+          'cpu-priority': cpupriority
           }
    with open("setip/set-cpu.json", "w") as set:
      json.dump(push, set, indent=4)
