b3ddb4e6cd862c95ca602e9afe49ed0c9f5776bd
saengx <157899625+saengx@users.noreply.github.com>
2025-09-16 05:10:51 +0700

    Update scan.py

--- mobile-mining/scan.py
+++ mobile-mining/scan.py
@@ -9,10 +9,9 @@ def scan_port(ip, port):
     except (socket.timeout, ConnectionRefusedError):
         return False
 
-# ตัวอย่างการสแกน IP ในเครือข่าย (ต้องระบุช่วง IP ที่ถูกต้อง)
 network_ip_prefix = "172.16.10." # เปลี่ยนเป็น IP prefix ของเครือข่ายคุณ
 
 for i in range(1, 255):
     ip_address = f"{network_ip_prefix}{i}"
-    scan_port(ip_address, 8080)  # ตรวจสอบพอร์ต 80 (HTTP)
+    scan_port(ip_address, 8080)  # ตรวจสอบพอร์ต 8080 (HTTP)
    
