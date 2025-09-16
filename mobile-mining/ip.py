8fea0614481350a861c3c40b8f445f5c3fe0cf89
saengx <157899625+saengx@users.noreply.github.com>
2025-09-17 06:31:07 +0700

    Update ip.py

--- mobile-mining/ip.py
+++ mobile-mining/ip.py
@@ -17,12 +17,12 @@ def setip():
 
     banner()
     try:
-        print("ตัวอย่าง:  \033[93m172.20.20.20:\033[00m")
-        ip = input("   ip   ")
+        print("ใส่ค่า IPprefix เฉพาะ 3 ชุดแรก ตัวอย่าง:  \033[93m192.168.1:\033[00m")
+        IPprefix = input("   IPprefix   ")
         print("\033[35m-----------------------------------------\033[0m")
 
 
-        if ip == "":
+        if IPprefix == "":
             raise Exception()
     except:
         os.system("@cls||clear")
@@ -31,23 +31,23 @@ def setip():
         os.system("python3 ip.py")
 
     push = {
-           'ip': ip
+           'IPprefix': IPprefix
            }
-    with open("setip/ipserver.json", "w") as set:
+    with open("setip/IPprefix.json", "w") as set:
         json.dump(push, set, indent=4)
 
 
 while True:
     banner()
     setip()
-    with open("setip/ipserver.json",encoding="utf-8") as set:
+    with open("setip/IPprefix.json",encoding="utf-8") as set:
         load = set.read()
         loads = json.loads(load)
-        ip = loads['ip']
+        IPprefix = loads['IPprefix']
 
         print("\033[36m")
-        print("ip address ที่บันทึก")
-        print("ip  =",ip)
+        print("IPprefix ที่บันทึก")
+        print("IPprefix  =",IPprefix)
         print("\033[0m\n")
         print("\033[31mโปรดตรวจสอบการตั้งค่า ถ้าถูกต้อง ให้ใช้คำสั่ง  run-miner  เพื่อเปิดขุด\033[0m")
     break
