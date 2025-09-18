4cb476191ac91037cf12ca2c8e487b6d7caa09ea
saengx <157899625+saengx@users.noreply.github.com>
2025-09-19 03:02:53 +0700

    Update scan.py

--- mobile-mining/scan.py
+++ mobile-mining/scan.py
@@ -2,7 +2,7 @@ import socket, json, os
 def scan_port(ips, port):
     try:
         sock = socket.create_connection((ips, port), timeout=0.1)
-        result = sock.connect(sa)
+        result = socket.connect(0)
         #sock.close()
         if result == 0:
             print(f"พบ HTTP Server ที่ {ips}:{port}")
