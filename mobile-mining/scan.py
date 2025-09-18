42a9a8febd8eaebd7a472200d111046e8356d62c
saengx <157899625+saengx@users.noreply.github.com>
2025-09-19 04:07:40 +0700

    Update scan.py

--- mobile-mining/scan.py
+++ mobile-mining/scan.py
@@ -16,7 +16,7 @@ def scan_port(ips, port):
         push = {'ip': f"{IPS}"}
         with open("setip/ipserver.json", "w") as set:
             json.dump(push, set, indent=4)
-        os.system("python3 check.py") 
+        sys.exit("python3 เทสระบบ") 
     except (socket.timeout, ConnectionRefusedError):
         print(f"{network_ip_prefix}.{i}")
         #return False 
