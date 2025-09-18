da53ee3cdb238394a7055b2957d9915e62c2cb52
saengx <157899625+saengx@users.noreply.github.com>
2025-09-19 04:30:41 +0700

    Update scan.py

--- mobile-mining/scan.py
+++ mobile-mining/scan.py
@@ -31,5 +31,5 @@ network_ip_prefix = f"{IPprefix}" # เปลี่ยนเป็น IP prefix 
 for i in range(2, 256):
     ip_address = f"{network_ip_prefix}.{i}"
     scan_port(ip_address, 8080)  # ตรวจสอบพอร์ต 8080 (HTTP)
-
+        break
 os.system("python3 scan.py")   
