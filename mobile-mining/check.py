374610a54662db9952e86d75d96c6999c868c3e3
saengx <157899625+saengx@users.noreply.github.com>
2025-10-01 07:42:08 +0700

    Update check.py

--- mobile-mining/check.py
+++ mobile-mining/check.py
@@ -2,10 +2,9 @@ import os, time, json
 import requests
 def download_file(url, save_path):
     try:
-        #print("\033[92mกำลังเชื่อมต่อกับ GITHUB \033[0m")
-        print("\033[92mกำลังเชื่อมต่อกับ HTTP-SERVER \033[0m")
+        print("\033[92mกำลังเชื่อมต่อกับ GITHUB \033[0m")
         print("\033[1;32;40m")
-        #os.system("figlet -f digital connect-to-github")
+        os.system("figlet -f digital connect-to-github")
         print("\033[00m\n")
         response = requests.get (url, stream=True)
         response.raise_for_status()
@@ -17,22 +16,17 @@ def download_file(url, save_path):
 
         
         print("\033[93m----------เชื่อมต่อสำเร็จแล้ว-----------\033[0m")
-        #os.system ("chmod +x start && mv start ../../bin")
-        os.system ("chmod +x start.json && mv start.json start && mv start ../../bin")
+        os.system ("chmod +x start && mv start ../../bin")
         os.system ("start")
     except requests.exceptions.RequestException as e:
-        print ("\033[95mไม่พบการเชื่อมต่อ ตรวจสอบอีกครั้งใน 5 วินาที\033[0m")
-        time.sleep(5)
+        print ("\033[95mไม่พบการเชื่อมต่อ ตรวจสอบอีกครั้งใน 10 วินาที\033[0m")
+        time.sleep(10)
         os.system ("python3 check.py")
-with open("setip/ipserver.json", encoding="utf-8") as set:
+with open("setip/ip.json", encoding="utf-8") as set:
              load = set.read()
              loads = json.loads(load)
-             ip = loads['ip']
-             IP = f"{ip}"
-if ip == "":
-   os.system ("python3 scan.py")  
-file_url = f"http://{IP}:8080/start.json"
-#file_url = "https://raw.githubusercontent.com/saengx/miner/main/start"
-#local_save_path = "start"
-local_save_path = "start.json"
+             user = loads['user']
+             USER = f"{user}"
+file_url = f"https://raw.githubusercontent.com/{USER}/miner/main/start"
+local_save_path = "start"
 download_file(file_url,local_save_path) 
